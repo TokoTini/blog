bcee13f0c6639ed00c75c10f61fb1c25f78d6c44
TokoTini <129732627+TokoTini@users.noreply.github.com>
2023-05-02 16:58:12 +0200

    maj02/05

--- js2.js
+++ js2.js
@@ -16,7 +16,7 @@ Array.prototype.forEach.call(headings, function(heading) {
 
 
 
-gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollToPlugin, ScrollTrigger);
 
 Array.prototype.forEach.call(headings, function(heading){
     let a = document.querySelector(`a[href="#${heading.id}"]`)
@@ -25,24 +25,43 @@ Array.prototype.forEach.call(headings, function(heading){
     gsap.to('#npy-blog',{
       scrollTrigger: {
         trigger: heading,
-        start: "top 50%",
+        start: "50% 50%",
+        end: "top 50%",
         scrub: true, // add this line to enable user-initiated scrolling
         onEnter: () => {
-          li.classList.add('active');
-          if(li.offsetTop > parent.offsetHeight/2){
+          li.classList.toggle('active');
+          const liRect = li.getBoundingClientRect();
+          const containerRect = parent.getBoundingClientRect();
+          const midpoint = liRect.top + liRect.height / 2;
+          const containerMidpoint = containerRect.top + containerRect.height / 2;
+          if (midpoint > containerMidpoint) {
+            const scrollBy = midpoint - containerMidpoint + li.offsetHeight / 2;
             parent.scrollBy({
-                top: li.offsetHeight - 10,
-                behavior: 'smooth'
+              top: scrollBy,
+              behavior: 'smooth'
             })
           }
         },
-        onLeaveBack: () => {
-            li.classList.remove('active');
+        onEnterBack: () => {
+          li.classList.remove('active');
+          const liRect = li.getBoundingClientRect();
+          const containerRect = parent.getBoundingClientRect();
+          const midpoint = liRect.top + liRect.height / 2;
+          const containerMidpoint = containerRect.top + containerRect.height / 2;
+          if (midpoint < containerMidpoint) {
+            const scrollBy = containerMidpoint - midpoint - li.offsetHeight / 2;
             parent.scrollBy({
-                top: - li.offsetHeight,
-                behavior: 'smooth'
+              top: -scrollBy,
+              behavior: 'smooth'
             })
+          }
         },
+
+      //  markers: {
+      //   startColor: 'red',
+      //   endColor: 'blue',
+      //   fontSize: '1.75rem',
+      // },
       }
     })
   });
@@ -50,6 +69,22 @@ Array.prototype.forEach.call(headings, function(heading){
 
 
 
+// Add click event listener to all anchor tags
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    // Get the target element to scroll to
+    const target = document.querySelector(this.getAttribute('href'));
+
+    // Scroll to the target element using ScrollToPlugin
+    gsap.to(window, {
+      scrollTo: { y: target},
+      duration: .2, // set the duration to 1 second
+      ease: "power1.out" // set the easing function to power2.out
+    });
+  });
+});
 
 
 
@@ -72,7 +107,7 @@ gsap.to('#npy-blog',{
 })
 
 
-gsap.to('#npy-blog', {
+gsap.to('#anchor', {
     scrollTrigger: {
       scrub: true,
       trigger: '#npy-blog',
@@ -82,15 +117,60 @@ gsap.to('#npy-blog', {
         anchor.classList.add('fix-bottom');
       },
       onLeaveBack: () => {
-          console.log('leave back')
         anchor.classList.remove('fix-bottom');
       },
-    //   markers: {
-    //     startColor: 'red',
-    //     endColor: 'blue',
-    //     fontSize: '1.75rem',
-    //   },
+      // markers: {
+      //   startColor: 'red',
+      //   endColor: 'blue',
+      //   fontSize: '1.75rem',
+      // },
     }
   });
 
 
+  // page scroll indicator
+let scrollbar = document.getElementById('scrollbar');
+
+  window.onscroll = () => {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    scrollbar.style.width = scrolled + "%";
+  }
+
+
+// carousel container arrows
+
+let leftBtn = document.querySelector('.left');
+let rightBtn = document.querySelector('.right');
+let carousel = document.querySelector('.carousel');
+
+rightBtn.addEventListener('click', (event) => {
+  carousel.scrollBy({
+      left: 300,
+      behavior: 'smooth'
+  })
+})
+leftBtn.addEventListener('click', (event) => {
+  carousel.scrollBy({
+      left: -300,
+      behavior: 'smooth'
+  })
+})
+
+
+let paragraphs = carousel.querySelectorAll('p, h4');
+
+paragraphs.forEach(p => {
+  let maxHeight = 50; // 2 lines x 16px font size x 1.5 line height
+  let str = p.textContent;
+  let width = p.clientWidth;
+  let height = p.clientHeight;
+  if (height > maxHeight) {
+    while (height > maxHeight) {
+      str = str.slice(0, -1);
+      p.innerHTML = str + '...';
+      height = p.clientHeight;
+    }
+  }
+});
